e44acc2bb4cf322139888ad71b850c7f1adb6097
ambegossi <andersonbegossi@gmail.com>
2020-01-15 11:43:26 -0300

    Added virtuals on SpotSchema

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -10,7 +13,9 @@ mongoose.connect('mongodb+srv://ambegossi:ambegossi@cluster0-pjivk.mongodb.net/a
 });
 
 // falando para o express utilizar formato json
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
