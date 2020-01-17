2df8b45940973b7097bc8b691d6e928acb90f79f
ambegossi <andersonbegossi@gmail.com>
2020-01-16 23:01:30 -0300

    Added websockets

--- src/server.js
+++ src/server.js
@@ -3,14 +3,37 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app); //Pegando servidor http e extraindo ele de dentro do express
+const io = socketio(server); //A partir disso, o server passa a também ouvir o protocolo websocket
+
+const connectedUsers = {};
+
+//socket representa a conexão com um novo usuário logado (web e mobile)
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    //relacionando o id de usuário com o id de conexão dele
+    connectedUsers[user_id] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://ambegossi:ambegossi@cluster0-pjivk.mongodb.net/aircncdb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+//use serve para adicionar uma funcionalidade em toda rota, independente se usa método get,pos...
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 // falando para o express utilizar formato json
 app.use(cors());
@@ -18,4 +41,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
